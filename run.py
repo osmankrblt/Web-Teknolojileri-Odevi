8a7f72950babe1f5a3b996fb068580832c4fb753
Osman Karabulut <h.osmankarabulut.fb67@hotmail.com>
2023-06-10 16:06:35 +0300

    new file:   __pycache__/forms.cpython-310.pyc 	modified:   run.py 	modified:   static/style.css 	new file:   templates/formhelpers.html 	modified:   templates/home_page.html 	new file:   templates/includes/messages.html 	modified:   templates/main_template.html 	new file:   templates/register.html

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,flash,redirect,url_for
+
+from forms import RegisterForm
 
 app = Flask(__name__, template_folder="templates")
 
@@ -20,10 +22,21 @@ def category_page(category):
 @app.route("/<category>/<product_id>")
 def product_page(category, product_id):
 
-    #products = get_urun()
+    #products = get_urun(product_id)
     product = 1
     return render_template("product_page.html", category_name=category, urun=product)
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        print(form)
+        #db_session.add(user)
+        flash('Başarıyla kayıt olundu...')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == "__main__":
 
